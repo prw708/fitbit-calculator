c13a2921de00f9c2351cc33636afa42aa2f59024
r945 <cdc70@case.edu>
2020-01-06 21:55:55 -0600

    Changed code for SDK 4.0 document.replaceSync feature

--- app/index.js
+++ app/index.js
@@ -5,29 +5,7 @@ import {addFloatingPoints,
         multiplyFloatingPoints, 
         divideFloatingPoints} from "./utils.js"
 import * as messaging from "messaging";
-
-let numberPad = document.getElementById("numberPad");
-let operatorPad = document.getElementById("operatorPad");
-let result = document.getElementById("result");
-
-let num1 = document.getElementById("1");
-let num2 = document.getElementById("2");
-let num3 = document.getElementById("3");
-let num4 = document.getElementById("4");
-let num5 = document.getElementById("5");
-let num6 = document.getElementById("6");
-let num7 = document.getElementById("7");
-let num8 = document.getElementById("8");
-let num9 = document.getElementById("9");
-let dot = document.getElementById("dot");
-let num0 = document.getElementById("0");
-let operatorElement = document.getElementById("operator");
-
-let add = document.getElementById("add");
-let subtract = document.getElementById("subtract");
-let multiply = document.getElementById("multiply");
-let divide = document.getElementById("divide");
-let clear = document.getElementById("clear");
+import {me as device} from "device";
 
 let memory = "";
 let operator = "";
@@ -38,43 +16,38 @@ const lowerLimit = -9999999;
 let numButtonColor = "fb-blue";
 let operatorButtonColor = "fb-lime";
 
+initNumScreen();
+
 /*
  *  Add onmousedown and onmouseup events as a UI effect for a click
  */
 
-let numKeys = document.getElementsByClassName("numberButton");
-numKeys.forEach(function(numKey) {
-  numKey.onmousedown = function(e) {
-    numKey.style.fill = "fb-black";
-    numKey.style.opacity = 0.6;
-  }
-  numKey.onmouseup = function(e) {
-    numKey.style.opacity = 0;
-  }
-});
+function setMouseEvents() {
+  let numKeys = document.getElementsByClassName("numberButton");
+  numKeys.forEach(function(numKey) {
+    numKey.onmousedown = function(e) {
+      numKey.style.fill = "fb-black";
+      numKey.style.opacity = 0.6;
+    }
+    numKey.onmouseup = function(e) {
+      numKey.style.opacity = 0;
+    }
+  });
 
-let operatorKeys = document.getElementsByClassName("operatorButton");
-operatorKeys.forEach(function(operatorKey) {
-  operatorKey.onmousedown = function(e) {
-    operatorKey.style.fill = "fb-black";
-    operatorKey.style.opacity = 0.6;
-  }
-  operatorKey.onmouseup = function(e) {
-    operatorKey.style.opacity = 0;
-  }
-});
-
-let clearKey = document.getElementById("clear");
-clearKey.onmousedown = function(e) {
-  clearKey.style.fill = "fb-black";
-  clearKey.style.opacity = 0.6;
-}
-clearKey.onmouseup = function(e) {
-  clearKey.style.opacity = 0;
+  let operatorKeys = document.getElementsByClassName("operatorButton");
+  operatorKeys.forEach(function(operatorKey) {
+    operatorKey.onmousedown = function(e) {
+      operatorKey.style.fill = "fb-black";
+      operatorKey.style.opacity = 0.6;
+    }
+    operatorKey.onmouseup = function(e) {
+      operatorKey.style.opacity = 0;
+    }
+  });
 }
 
 /*
- *  Reset color scheme based on setting
+ *  Set color scheme based on setting
  */
 
 messaging.peerSocket.onmessage = function(evt) {
@@ -100,110 +73,155 @@ messaging.peerSocket.onmessage = function(evt) {
       operatorButtonColor = "fb-red";
       break;
   }
+  let numKeys = document.getElementsByClassName("numberButton");
   numKeys.forEach(function(numKey) {
     numKey.style.fill = numButtonColor;
   });
+  let operatorKeys = document.getElementsByClassName("operatorButton");
   operatorKeys.forEach(function(operatorKey) {
     operatorKey.style.fill = operatorButtonColor;
   });
 }
 
 /*
- *  Process the number clicks
+ *  Process number screen events
  */
 
-num1.onclick = function(e) {
-  operand = operand.concat("1");
-}
-num2.onclick = function(e) {
-  operand = operand.concat("2");
-}
-num3.onclick = function(e) {
-  operand = operand.concat("3");
-}
-num4.onclick = function(e) {
-  operand = operand.concat("4");
-}
-num5.onclick = function(e) {
-  operand = operand.concat("5");
-}
-num6.onclick = function(e) {
-  operand = operand.concat("6");
-}
-num7.onclick = function(e) {
-  operand = operand.concat("7");
-}
-num8.onclick = function(e) {
-  operand = operand.concat("8");
-}
-num9.onclick = function(e) {
-  operand = operand.concat("9");
-}
-dot.onclick = function(e) {
-  operand = operand.concat(".");
-}
-num0.onclick = function(e) {
-  operand = operand.concat("0");
+function initNumScreen() {
+  setMouseEvents();
+  let num1 = document.getElementById("1");
+  let num2 = document.getElementById("2");
+  let num3 = document.getElementById("3");
+  let num4 = document.getElementById("4");
+  let num5 = document.getElementById("5");
+  let num6 = document.getElementById("6");
+  let num7 = document.getElementById("7");
+  let num8 = document.getElementById("8");
+  let num9 = document.getElementById("9");
+  let dot = document.getElementById("dot");
+  let num0 = document.getElementById("0");
+  let operatorElement = document.getElementById("operator");
+
+  num1.onclick = function(e) {
+    operand = operand.concat("1");
+  }
+  num2.onclick = function(e) {
+    operand = operand.concat("2");
+  }
+  num3.onclick = function(e) {
+    operand = operand.concat("3");
+  }
+  num4.onclick = function(e) {
+    operand = operand.concat("4");
+  }
+  num5.onclick = function(e) {
+    operand = operand.concat("5");
+  }
+  num6.onclick = function(e) {
+    operand = operand.concat("6");
+  }
+  num7.onclick = function(e) {
+    operand = operand.concat("7");
+  }
+  num8.onclick = function(e) {
+    operand = operand.concat("8");
+  }
+  num9.onclick = function(e) {
+    operand = operand.concat("9");
+  }
+  dot.onclick = function(e) {
+    operand = operand.concat(".");
+  }
+  num0.onclick = function(e) {
+    operand = operand.concat("0");
+  }
+  operatorElement.onclick = function(e) {
+    document.replaceSync("operator_screen.gui");
+    initOperatorScreen();
+    operand = parseFloat(operand);
+    if (memory === "") {
+      memory = operand;
+    } else {
+      switch (operator) {
+        case "+":
+          memory = addFloatingPoints(memory, operand);
+          break;
+        case "-":
+          memory = subtractFloatingPoints(memory, operand);
+          break;
+        case "*":
+          memory = multiplyFloatingPoints(memory, operand);
+          break;
+        case "/":
+          memory = divideFloatingPoints(memory, operand);
+          break;
+      }
+    }
+    operand = "";
+    let result = document.getElementById("result");
+    if (memory > upperLimit || memory < lowerLimit) {
+      result.text = "OVERFLOW";
+    } else {
+      result.text = monospaceDigits(memory);
+    }
+  }
 }
 
 /*
- *  Process operator clicks
+ *  Process operator screen events
  */
 
-operatorElement.onclick = function(e) {
-  numberPad.style.display = "none";
-  operatorPad.style.display = "inline";
-  operand = parseFloat(operand);
-  if (memory === "") {
-    memory = operand;
-  } else {
-    switch (operator) {
-      case "+":
-        memory = addFloatingPoints(memory, operand);
-        break;
-      case "-":
-        memory = subtractFloatingPoints(memory, operand);
-        break;
-      case "*":
-        memory = multiplyFloatingPoints(memory, operand);
-        break;
-      case "/":
-        memory = divideFloatingPoints(memory, operand);
-        break;
-    }
+function initOperatorScreen() {
+  setMouseEvents();
+  let add = document.getElementById("add");
+  let subtract = document.getElementById("subtract");
+  let multiply = document.getElementById("multiply");
+  let divide = document.getElementById("divide");
+  let clear = document.getElementById("clear");
+  let result = document.getElementById("result");
+
+  let clearKey = document.getElementById("clear");
+  clearKey.onmousedown = function(e) {
+    clearKey.style.fill = "fb-black";
+    clearKey.style.opacity = 0.6;
   }
-  operand = "";
-  if (memory > upperLimit || memory < lowerLimit) {
-    result.text = "OVERFLOW";
-  } else {
-    result.text = monospaceDigits(memory);
+  clearKey.onmouseup = function(e) {
+    clearKey.style.opacity = 0;
+  }
+  
+  add.onclick = function(e) {
+    document.replaceSync("index.gui");
+    initNumScreen();
+    operator = "+";
+  }
+  subtract.onclick = function(e) {
+    document.replaceSync("index.gui");
+    initNumScreen();
+    operator = "-";
+  }
+  multiply.onclick = function(e) {
+    document.replaceSync("index.gui");
+    initNumScreen();
+    operator = "*";
+  }
+  divide.onclick = function(e) {
+    document.replaceSync("index.gui");
+    initNumScreen();
+    operator = "/";
+  }
+  clear.onclick = function(e) {
+    result.text = "";
+    document.replaceSync("index.gui");
+    initNumScreen();
+    memory = "";
+    operand = "";
+    operator = "";
   }
 }
-add.onclick = function(e) {
-  numberPad.style.display = "inline";
-  operatorPad.style.display = "none";
-  operator = "+";
-}
-subtract.onclick = function(e) {
-  numberPad.style.display = "inline";
-  operatorPad.style.display = "none";
-  operator = "-";
-}
-multiply.onclick = function(e) {
-  numberPad.style.display = "inline";
-  operatorPad.style.display = "none";
-  operator = "*";
-}
-divide.onclick = function(e) {
-  numberPad.style.display = "inline";
-  operatorPad.style.display = "none";
-  operator = "/";
-}
-clear.onclick = function(e) {
-  numberPad.style.display = "inline";
-  operatorPad.style.display = "none";
-  memory = "";
-  operand = "";
-  operator = "";
-  result.text = "";
-}
+
+
+
+
+
+
+
